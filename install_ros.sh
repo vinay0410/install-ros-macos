7be4d19c1bc36718e9111635499e38493f3f5d02
Vinay Sharma <vinay04sharma@icloud.com>
2018-09-26 01:47:24 +0530

    first push

---
+++ install_ros.sh
@@ -0,0 +1,30 @@
+brew update
+brew install cmake
+
+brew tap ros/deps
+brew tap osrf/simulation   # Gazebo, sdformat, and ogre
+brew tap homebrew/core # VTK5
+brew tap homebrew/science  # others
+
+mkdir -p ~/Library/Python/2.7/lib/python/site-packages
+echo "$(brew --prefix)/lib/python2.7/site-packages" >> ~/Library/Python/2.7/lib/python/site-packages/homebrew.pth
+
+sudo -H python2 -m pip install wxPython # Install python-wxtools
+
+sudo -H python2 -m pip install -U wstool rosdep rosinstall rosinstall_generator rospkg catkin-pkg sphinx
+
+sudo -H rosdep init
+rosdep update
+
+mkdir ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+
+rosinstall_generator desktop --rosdistro lunar --deps --wet-only --tar > lunar-desktop-wet.rosinstall
+
+wstool init -j8 src lunar-desktop-wet.rosinstall
+
+rosdep install --skip-keys google-mock --from-paths src --ignore-src --rosdistro lunar -y
+
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+
+source ~/ros_catkin_ws/install_isolated/setup.bash
